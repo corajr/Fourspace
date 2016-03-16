bc07362106dfd35601a67dd5410b66090cfeb604
Cora Johnson-Roberson <me@corajr.com>
2016-03-16 14:43:05 -0400

    re-export more modules

--- src/fourspace.js
+++ src/fourspace.js
@@ -1,7 +1,15 @@
+import FourspaceGeometry from './fourspace-geometry.js';
+import TesseractGeometry from './geometries/tesseract.js';
+import FourspaceShader from './shaders/FourspaceShader.js';
+import { applyMatrixToVec4Array } from './math/Matrix.js';
 import Box4 from './math/Box4.js';
 
 const Fourspace = {
-  Box4
+  Box4,
+  FourspaceGeometry,
+  TesseractGeometry,
+  FourspaceShader,
+  applyMatrixToVec4Array
 };
 
 export default Fourspace;
--- test/unit/fourspace.js
+++ test/unit/fourspace.js
@@ -3,5 +3,9 @@ import Fourspace from '../../src/fourspace';
 describe('Fourspace', () => {
   it('should re-export other modules', () => {
     expect(Fourspace.Box4).to.exist;
+    expect(Fourspace.FourspaceGeometry).to.exist;
+    expect(Fourspace.TesseractGeometry).to.exist;
+    expect(Fourspace.FourspaceShader).to.exist;
+    expect(Fourspace.applyMatrixToVec4Array).to.exist;
   });
 });
